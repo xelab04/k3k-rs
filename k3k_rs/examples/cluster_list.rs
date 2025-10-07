bc19398517adae767603e5d637c5eb0b3b849274
alex <alexbissessur@gmail.com>
2025-10-07 14:35:12 +0400

    use fn to shorten cluster list

--- k3k_rs/examples/cluster_list.rs
+++ k3k_rs/examples/cluster_list.rs
@@ -1,30 +1,16 @@
 use kube::{Api, Client};
-use k3k_rs::cluster::Cluster;
+use k3k_rs::cluster;
 use kube::api::ListParams;
 use serde_yaml;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // Connect to cluster (KUBECONFIG or in-cluster)
-    let client = Client::try_default().await.unwrap();
 
-    // Api for namespaced CRDs
-    let clusters: Api<Cluster> = Api::all(client); // adjust namespace
+    let client = Client::try_default().await?;
+    let list = cluster::list(&client, "k3k-namespace").await?;
 
-    // List all Cluster objects
-    let lp = ListParams::default();
-    let list = clusters.list(&lp).await.unwrap();
-
-    println!("Found {} clusters:", list.items.len());
-    // for c in list.items {
-    //     let name = c.metadata.name.unwrap_or_default();
-    //     let servers = c.spec.servers;
-    //     println!(" - {} ({} server(s))", name, servers);
-    // }
-
-    for c in list.items {
+    for c in list {
         let yaml = serde_yaml::to_string(&c)?;
-
         println!("---\n{}", yaml);
     }
 
