e49449d71bf394ef0d65af78e317491aec475b19
xelab04 <baichoo04@gmail.com>
2025-10-11 15:07:49 +0400

    add easy cluster creation

--- k3k_rs/src/cluster/cluster_fn.rs
+++ k3k_rs/src/cluster/cluster_fn.rs
@@ -1,8 +1,9 @@
-use kube::{Api, Client, Error as KubeError };
-use kube::api::{PostParams, DeleteParams};
-use k8s_openapi::api::core::v1::Namespace;
+use crate::cluster;
 use crate::cluster::Cluster;
 use crate::namespace;
+use k8s_openapi::api::core::v1::Namespace;
+use kube::api::{DeleteParams, PostParams};
+use kube::{Api, Client, Error as KubeError};
 
 pub mod list {
     use super::*;
@@ -15,14 +16,15 @@ pub mod list {
     }
 }
 
-pub async fn cluster_list(client: &Client, namespace: Option<&str>) -> anyhow::Result<Vec<Cluster>> {
-
+pub async fn cluster_list(
+    client: &Client,
+    namespace: Option<&str>,
+) -> anyhow::Result<Vec<Cluster>> {
     let api: Api<Cluster>;
     if let Some(namespace) = namespace {
         api = Api::namespaced(client.clone(), namespace);
-    }
-    else {
-         api = Api::all(client.clone());
+    } else {
+        api = Api::all(client.clone());
     }
 
     let list = api.list(&Default::default()).await?;
@@ -35,19 +37,68 @@ pub async fn get(client: &Client, namespace: &str, name: &str) -> anyhow::Result
     Ok(obj)
 }
 
-pub async fn create(client: &Client, namespace: &str, cluster: &Cluster) -> anyhow::Result<Cluster> {
+pub async fn create_easy(client: &Client, cluster_name: &str) -> anyhow::Result<Cluster> {
+    let cluster_namespace = format!("k3k-{}", cluster_name);
+
+    let cluster_schema = Cluster {
+        metadata: kube::core::ObjectMeta {
+            name: Some(cluster_name.to_string()),
+            namespace: Some(cluster_namespace.to_string()),
+            ..Default::default()
+        },
+        spec: cluster::ClusterSpec {
+            servers: 1,
+            agents: 0,
+            mode: "shared".to_string(),
+            persistence: Some(cluster::PersistenceSpec {
+                r#type: Some("dynamic".to_string()),
+                storageClassName: None,
+                storageRequestSize: Some("2G".to_string()),
+            }),
+            expose: Some(cluster::ExposeSpec {
+                LoadBalancer: Some(cluster::ExposeLoadBalancer {
+                    etcd_port: Some(2379),
+                    server_port: Some(443),
+                }),
+                NodePort: None,
+                Ingress: None,
+            }),
+            sync: Some(cluster::SyncSpec {
+                ingresses: Some(cluster::SyncResourceSpec {
+                    enabled: true,
+                    selector: None,
+                }),
+                ..Default::default()
+            }),
+            ..Default::default()
+        },
+        status: None,
+    };
+
+    let api: Api<Cluster> = Api::namespaced(client.clone(), &cluster_namespace);
+    let mut pp = PostParams::default();
+    pp.dry_run = true;
+    let obj = api.create(&pp, &cluster_schema).await?;
+    Ok(obj)
+}
+
+pub async fn create(
+    client: &Client,
+    namespace: &str,
+    cluster: &Cluster,
+) -> anyhow::Result<Cluster> {
     let ns_api: Api<Namespace> = Api::all(client.clone());
     match ns_api.get(namespace).await {
         Ok(ns) => {
             println!("Namespace found: {}", ns.metadata.name.unwrap());
-        },
+        }
         Err(KubeError::Api(error_response)) if error_response.code == 404 => {
             println!("Namespace not found: {}", error_response.message);
             namespace::create_easy(&client, &namespace).await?;
-        },
+        }
         Err(err) => {
             println!("Unexpected error: {}", err);
-        },
+        }
     };
 
     let api: Api<Cluster> = Api::namespaced(client.clone(), namespace);
