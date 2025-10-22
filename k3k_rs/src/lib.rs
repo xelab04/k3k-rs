90e27b6ce9c84143e6f642304f9a0f2c46c76cc4
alex <alexbissessur@gmail.com>
2025-10-22 13:42:51 +0400

    add required files to module

--- k3k_rs/src/lib.rs
+++ k3k_rs/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod cluster;
 pub mod namespace;
+pub mod virtualclusterpolicy;
---
+++ k3k_rs/src/virtualclusterpolicy/mod.rs
@@ -0,0 +1,5 @@
+pub mod virtualclusterpolicy_crd;
+pub mod virtualclusterpolicy_fn;
+
+pub use virtualclusterpolicy_crd::*;
+pub use virtualclusterpolicy_fn::*;
